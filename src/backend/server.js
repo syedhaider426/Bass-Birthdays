954057a3d99dfb3089358bff874d0feb8f42a93a
Syed Haider <shayder426@gmail.com>
2020-01-12 15:27:37 -0800

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,13 +1,6 @@
 //Starts up server
 
 const env = process.env.NODE_ENV || "development";
-console.log("Current environment", env);
-console.error("Current environment", env);
-if (env == "production") {
-  process.env.NODE_CONFIG_DIR = "../backend/config";
-} else {
-  process.env.NODE_CONFIG_DIR = "./config";
-}
 
 const express = require("express");
 const app = express();
